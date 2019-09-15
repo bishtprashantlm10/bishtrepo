253c4c6980f70f766d5a36814425884a0e25b061
Leo Messi <leomessi10@gmail.com>
2019-09-15 03:28:37 -0400

    Replicating merge conflictt

--- calc.py
+++ calc.py
@@ -2,11 +2,10 @@ def add(x,y,z):
     pass
 def subtract(x,y):
     pass
-def multiply(x,y,z,z):
+def multiply(x,y,z,z,ddddddddddddd):
     return x*y
 def divide(x,y,3,3):
     pass
-def divide(x,y,3,3,4,4,5,6):
 def square(x,y):
     pass
 
